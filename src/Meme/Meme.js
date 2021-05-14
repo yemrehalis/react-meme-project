90b873de9e8eca2a67b0007052f1e6227fc26be1
Yunus Emre Halis <yemrehalis@gmail.com>
2021-05-14 13:20:46 +0300

    get random memes from API , get Captions Inputs count per meme

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -5,19 +5,43 @@ export const Meme = () => {
 
     const [memes, setMemes] = useState([]);
     const [memeIndex, setMemeIndex] = useState(0);
+    const [captions, setCaptions] = useState([]);
+
+    const shuffleMemes = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i);
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes').then(res => res.json().then(res => {
             console.log(res);
-            const memes = res.data.memes;
-            setMemes(memes);
+            const _memes = res.data.memes;
+            shuffleMemes(_memes);
+            setMemes(_memes);
         }));
     }, []);
+
+    useEffect(() => {
+        console.log(captions);
+    }, [captions])
+
+
+
+    useEffect(() => {
+        if (memes.length) {
+            setCaptions(Array(memes[memeIndex].box_count).fill(''));
+        }
+    }, [memeIndex, memes]);
 
     return (
 
         memes.length ?
             <div className={styles.container}>
+                <button onClick={() => console.log('generate')} className={styles.generate} >Generate</button>
                 <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip} >Skip</button>
                 <img src={memes[memeIndex].url} alt="" />
 
